682510766d52459ea73c729bb3d1c5e677afbd52
Michael Schloss <mschloss11@gmail.com>
2026-03-30 10:38:10 -0500

    linux-crypto branch

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         .package(url: "https://github.com/vapor/vapor.git", from: "4.119.0"),
         .package(url: "https://github.com/vapor/fluent.git", from: "4.13.0"),
         .package(url: "https://github.com/vapor/fluent-sqlite-driver.git", from: "4.0.0"),
-        .package(url: "https://github.com/schlossm/passage.git", branch: "main"),
+        .package(url: "https://github.com/schlossm/passage.git", branch: "linux-crypto"),
         .package(url: "https://github.com/apple/swift-testing.git", branch: "main"),
     ],
     targets: [
